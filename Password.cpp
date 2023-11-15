17372f78c1e41f59666d1a881a619b3c3bb95ed2
tesss24 <102249475+tesss24@users.noreply.github.com>
2023-11-15 11:53:17 -0800

    Update Password.cpp

--- Password.cpp
+++ Password.cpp
@@ -1,7 +1,15 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
+#include<iostream>
 
 using std::string;
+using std::cout;
+using std::endl;
+
+Password::Password(){
+  pass_history.push_back("ChicoCA-95929");
+}
 
 /*
   The function receives a string counts how many times the same character 
@@ -24,7 +32,7 @@ bool Password::has_mixed_case(string password){
   int upper = false;
   int lower = false;
   int index = 0;
-  while( index < password.length()-1 ){
+  while( index <= password.length()-1 ){
     if (isupper(password[index])){
       upper = true;
     }
@@ -38,3 +46,37 @@ bool Password::has_mixed_case(string password){
   }
   return false;
 }
+
+void Password::set(string password){
+  if (password.length() < 8){
+    cout << "Too short" << endl;
+    return;
+  }
+  int count = count_leading_characters(password);
+  if (count > 3){
+    cout << "Can't have more then three of the same leading character" << endl;
+    return;
+  }
+  if(!has_mixed_case(password)){
+    cout << "Needs at least one upper case and one lower case letter" << endl;
+    return;
+  }
+  for( int i = 0; i < pass_history.size(); i++){
+    if (password == pass_history[i]){
+      cout << "Can't be an already used password" << endl;
+      return;
+    }
+  }
+  pass_history.push_back(password);
+  return;
+}
+
+bool Password::authenticate(string password){
+  if(pass_history.size() > 0){
+    if (password == pass_history[pass_history.size()-1]){
+      return true;
+    }
+  }
+  return false;
+}
+
